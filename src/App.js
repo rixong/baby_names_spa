e30fb1e0d3e7cf1c3eefc2dffd11b3097a55e736
rixong <rixong@gmail.com>
2020-08-23 13:51:17 -0500

    trim name

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ const App = () => {
       body:
         JSON.stringify({
           uid: curId,
-          name: name
+          name: name.trim()
         })
     })
     const response = await result.json()
