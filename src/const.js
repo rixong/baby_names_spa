910ea40a3d293193335da632738c147d2b8cd7af
rixong <rixong@gmail.com>
2020-09-02 08:38:24 -0500

    debug deployment - url const

--- src/const.js
+++ src/const.js
@@ -1,24 +1,14 @@
 const prod = {
   url: {
-<<<<<<< HEAD
     API_URL: 'https://rixong-baby-spa.herokuapp.com/'
     // CLIENT_URL: 'https://pensive-villani-db112d.netlify.app/'
-=======
-    API_URL: 'https://rixong-baby-spa.herokuapp.com/',
-    CLIENT_URL: 'https://pensive-villani-db112d.netlify.app/'
->>>>>>> 185511592d124142487da505adfea635d7d0be74
   }
 };
 
 const dev = {
   url: {
-<<<<<<< HEAD
     API_URL: 'http://localhost:3000'
     // CLIENT_URL: 'http://localhost:3001'
-=======
-    API_URL: 'http://localhost:3000',
-    CLIENT_URL: 'http://localhost:3001'
->>>>>>> 185511592d124142487da505adfea635d7d0be74
   }
 };
 
